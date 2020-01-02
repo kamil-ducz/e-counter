89cb3a457624785ac28a5892159a3e42147a4905
kamil-ducz <kamilducz1989@gmail.com>
2020-01-02 17:21:18 +0100

    Add files via upload
    registration form validation added

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 getAndCalcData();
-    
+setInterval(function(){ getAndCalcData(); }, 30000);
+
+function unhidePassword() {
+  var x = document.getElementById("password");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
+
+
 function getAndCalcData() 
 {
     
@@ -18,7 +29,6 @@ $.ajax({
 	var id = data[0];              //get id
 	var vname = data[4];           //get name
     var vwallet = data[6];
-	console.log("dupsko");
 	//--------------------------------------------------------------------
 	// 3) Update html content
 	//--------------------------------------------------------------------
@@ -37,184 +47,65 @@ $.ajax({
             
             $("#lastUpdate").text("Last update: " +  result + "(GMT+1)");		// write in div with id "last update" (Time:GMT)
             
+
             for (i = 0; i < data2.items.length; i++)			// fill the left table
             {
                 $("#code" + (i + 1)).text(data2.items[i].code );
                 $("#unit" + (i + 1)).text(data2.items[i].unit );
-                $("#purchasePrice" + (i + 1)).text(data2.items[i].purchasePrice);
+                $("#sellPrice" + (i + 1)).text(data2.items[i].purchasePrice);
+                
             }
+
+            $('input[name=sellPriceUSD]').val(data2.items[0].sellPrice);
+            $('input[name=sellPriceEUR]').val(data2.items[1].sellPrice);
+            $('input[name=sellPriceCHF]').val(data2.items[2].sellPrice);
+            $('input[name=sellPriceRUB]').val(data2.items[3].sellPrice);
+            $('input[name=sellPriceCZK]').val(data2.items[4].sellPrice);
+            $('input[name=sellPriceGBP]').val(data2.items[5].sellPrice);
+            $('input[name=buyPriceUSD]').val(data2.items[0].purchasePrice);
+            $('input[name=buyPriceEUR]').val(data2.items[1].purchasePrice);
+            $('input[name=buyPriceCHF]').val(data2.items[2].purchasePrice);
+            $('input[name=buyPriceRUB]').val(data2.items[3].purchasePrice);
+            $('input[name=buyPriceCZK]').val(data2.items[4].purchasePrice);
+            $('input[name=buyPriceGBP]').val(data2.items[5].purchasePrice);
             
             for (i = 0; i < data2.items.length; i++)		//fill the right table - sellPrice column
             {
                 $("#codeSale" + (i + 1)).text(data2.items[i].code );
-                $("#sellPrice" + (i + 1)).text(data2.items[i].sellPrice );
+                $("#purchasePrice" + (i + 1)).text(data2.items[i].sellPrice );
             }
 
-            for(i = 0; i < 6; i++)  //fill the right table - amount column
+            
+            for(i = 0; i < data2.items.length; i++)  //fill the right table - amount and value column
             {
                 $("#amountWallet" + (i + 1)).text(data[i+6]);
-                $("#walletValue" + (i + 1)).text(data[i+6] * data2.items[i].sellPrice)
+                $("#walletValue" + (i + 1)).text(parseFloat(data[i+6] * data2.items[i].sellPrice).toFixed(4));
             }
 
+            var totalPLN = 0;
+			console.log("totalPLN: " + totalPLN);
+            var currentCurrencyValue = [];
 
-            
-            
-            
-
+            for(i = 0; i < data2.items.length; i++)  //sum all currecies amount value with walletPLN
+            {                
+                currentCurrencyValue[i] = data[i+6] * data2.items[i].sellPrice;
+                console.log("currentCurrencyValue[i]: " + currentCurrencyValue[i]);
+                totalPLN += currentCurrencyValue[i];
+				console.log("totalPLN: " + totalPLN);
+				
+            }
+			totalPLN += data[12] * 1; //parseFloat
+			console.log("totalPLN: " + totalPLN);
+            $("#totalPLN").text(parseFloat(totalPLN.toFixed(4)));
             
         })
         
         .fail(function() {		//if not connected
-            alert( "Can't connect to the server." );
+            alert( "Can't connect to the server" );
         });
   
   } 
 });
 
+
 };
-
-
-
-
-/*
-setTimeout(function(){ $("#hiddenForm").submit();clearTimeout() }, 300000);
-
-refreshData();
-setInterval("refreshData()", 100000);
-
-function refreshData()
-{
-
-	   $.getJSON( "proxy.php")		// was var jqxhr = $.getJSON but working now
-	  .done(function(data) {			//if connection ok
-		//get date and time from string, use split func to seperate string
-		var res1 = data.publicationDate.split("T");
-		var res2 = res1[1].split(".");	
-		var result = res1[0] + " " + res2[0];		
-		
-		$("#lastUpdate").text("Last update: " +  result + "(GMT+1)");		// write in div with id "last update" (Time:GMT)
-		
-		for (i = 0; i < data.items.length; i++)			// fill the left table
-		{
-			$("#code" + (i + 1)).text(data.items[i].code );
-			$("#unit" + (i + 1)).text(data.items[i].unit );
-			$("#purchasePrice" + (i + 1)).text(data.items[i].purchasePrice);
-		}
-		
-		for (i = 0; i < data.items.length; i++)		//fill the right table
-		{
-			$("#codeb" + (i + 1)).text(data.items[i].code );
-			$("#sellPrice" + (i + 1)).text(data.items[i].sellPrice );
-		}           
-
-		
-	  })
-	  .fail(function() {		//if not connected
-		alert( "Can't connect to the server." );
-	  });
-
-}
-
-// selling
-function operationsUSD()
-{
-    var ileUSD = prompt('How much do you want to sell?','100');
-    document.sellForm.sellUSD.value = ileUSD;
-    document.sellForm.submit();
-    sendCurrencies();
-
-}
-
-function operationsEUR()
-{
-    var ileEUR = prompt('How much do you want to sell?','100');
-    document.sellForm.sellEUR.value = ileEUR;
-    document.sellForm.submit();
-    document.hiddenForm.submit();
-}
-
-function operationsCHF()
-{
-    var ileCHF = prompt('How much do you want to sell?','100');
-    document.sellForm.sellCHF.value = ileCHF;
-    document.sellForm.submit();
-    document.hiddenForm.submit();
-}
-
-function operationsRUB()
-{
-    var ileRUB = prompt('How much do you want to sell?','100');
-    document.sellForm.sellRUB.value = ileRUB;
-    document.sellForm.submit();
-    document.hiddenForm.submit();
-}
-
-function operationsCZK()
-{
-    var ileCZK = prompt('How much do you want to sell?','100');
-    document.sellForm.sellCZK.value = ileCZK;
-    document.sellForm.submit();
-    document.hiddenForm.submit();
-}
-
-function operationsGBP()
-{
-    var ileGBP = prompt('How much do you want to sell?','100');
-    document.sellForm.sellGBP.value = ileGBP;
-    document.sellForm.submit();
-    document.hiddenForm.submit();
-
-}
-//2 means operations - buttons for buying
-function operationsUSD2()
-{
-    var ileUSD2 = prompt('How much do you want to sell?','100');
-    document.buyForm.buyUSD.value = ileUSD2;
-    document.buyForm.submit();
-    document.hiddenForm.submit();
-}
-
-function operationsEUR2()
-{
-    var ileEUR2 = prompt('How much do you want to buy?','100');
-    document.buyForm.buyEUR.value = ileEUR2;
-
-    document.buyForm.submit();
-    document.hiddenForm.submit();
-}
-
-function operationsCHF2()
-{
-    var ileCHF2 = prompt('How much do you want to buy?','100');
-    document.buyForm.buyCHF.value = ileCHF2;
-
-    document.buyForm.submit();
-    document.hiddenForm.submit();
-}
-
-function operationsRUB2()
-{
-    var ileRUB2 = prompt('How much do you want to buy?','100');
-    document.buyForm.buyRUB.value = ileRUB2;
-
-    document.buyForm.submit();
-    document.hiddenForm.submit();
-}
-
-function operationsCZK2()
-{
-    var ileCZK2 = prompt('How much do you want to buy?','100');
-    document.buyForm.buyCZK.value = ileCZK2;
-
-    document.buyForm.submit();
-    document.hiddenForm.submit();
-}
-function operationsGBP2()
-{
-    var ileGBP2 = prompt('How much do you want to buy?','100');
-    document.buyForm.buyGBP.value = ileGBP2;
-
-    document.buyForm.submit();
-    document.hiddenForm.submit();
-}
-*/
