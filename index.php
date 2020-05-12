1710e762c60acfa66ee84c291561bca5e5991999
kamil-ducz <kamilducz1989@gmail.com>
2020-05-12 16:32:10 +0200

    pathx fixed, db name fixed

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     if(isset($_SESSION['login'])) $logged = $_SESSION['login']; //logged
     require_once('connect.php');
     $link = mysqli_connect($host, $db_user, $db_password);
-    mysqli_select_db($link, "usersdatabase");
+    mysqli_select_db($link, "ecounter_usersdatabase");
 ?>
 
 <!DOCTYPE HTML>
@@ -71,7 +71,7 @@
     </thead>
     <tbody>
         <tr class="table-active">
-            <form name="buyUSDForm" method="POST" action="../e-counter-kamil-ducz-async/BuyOperations/buyOperationUSD.php">
+            <form name="buyUSDForm" method="POST" action="BuyOperations/buyOperationUSD.php">
                 <td id="code1"></td>
                 <td id="purchasePrice1"></td><input type="hidden" name="buyPriceUSD">                     
                 <td>
@@ -89,7 +89,7 @@
             </form>
         </tr>
         <tr>
-            <form name="buyEURForm" method="POST" action="../e-counter-kamil-ducz-async/BuyOperations/buyOperationEUR.php">
+            <form name="buyEURForm" method="POST" action="BuyOperations/buyOperationEUR.php">
 
                 <td id="code2"></td>
                 <td id="purchasePrice2"></td><input type="hidden" name="buyPriceEUR">                     
@@ -100,7 +100,7 @@
             </form>
         </tr>
         <tr>
-            <form name="buyCHFForm" method="POST" action="../e-counter-kamil-ducz-async/BuyOperations/buyOperationCHF.php">
+            <form name="buyCHFForm" method="POST" action="BuyOperations/buyOperationCHF.php">
 
                 <td id="code3"></td>
                 <td id="purchasePrice3"></td><input type="hidden" name="buyPriceCHF">                     
@@ -111,7 +111,7 @@
             </form>
         </tr>
         <tr>
-            <form name="buyRUBForm" method="POST" action="../e-counter-kamil-ducz-async/BuyOperations/buyOperationRUB.php">
+            <form name="buyRUBForm" method="POST" action="BuyOperations/buyOperationRUB.php">
 
                 <td id="code4"></td>
                 <td id="purchasePrice4"></td><input type="hidden" name="buyPriceRUB">                     
@@ -122,7 +122,7 @@
             </form>
         </tr>
         <tr>
-            <form name="buyCZKForm" method="POST" action="../e-counter-kamil-ducz-async/BuyOperations/buyOperationCZK.php">
+            <form name="buyCZKForm" method="POST" action="BuyOperations/buyOperationCZK.php">
 
                 <td id="code5"></td>
                 <td id="purchasePrice5"></td><input type="hidden" name="buyPriceCZK">                     
@@ -133,7 +133,7 @@
             </form>
         </tr>
         <tr>
-            <form name="buyGBPForm" method="POST" action="../e-counter-kamil-ducz-async/BuyOperations/buyOperationGBP.php">
+            <form name="buyGBPForm" method="POST" action="BuyOperations/buyOperationGBP.php">
 
                 <td id="code6"></td>
                 <td id="purchasePrice6"></td><input type="hidden" name="buyPriceGBP">                     
@@ -159,7 +159,7 @@
     </thead>
     <tbody>
         <tr>
-            <form name="sellUSDForm" method="POST" action="../e-counter-kamil-ducz-async/SellOperations/sellOperationUSD.php">
+            <form name="sellUSDForm" method="POST" action="SellOperations/sellOperationUSD.php">
             <td id="codeSale1"></td>
             <td id="sellPrice1"></td><input type="hidden" name="sellPriceUSD">                     
             <td id="amountWallet1"></td>
@@ -171,7 +171,7 @@
             </form>
         </tr>
         <tr>
-            <form name="sellEURForm" method="POST" action="../e-counter-kamil-ducz-async/SellOperations/sellOperationEUR.php">
+            <form name="sellEURForm" method="POST" action="SellOperations/sellOperationEUR.php">
 
                 <td id="codeSale2"></td>
                 <td id="sellPrice2"></td><input type="hidden" name="sellPriceEUR">                     
@@ -185,7 +185,7 @@
         </tr>
 
         <tr>
-            <form name="sellCHFForm" method="POST" action="../e-counter-kamil-ducz-async/SellOperations/sellOperationCHF.php">
+            <form name="sellCHFForm" method="POST" action="SellOperations/sellOperationCHF.php">
 
                 <td id="codeSale3"></td>
                 <td id="sellPrice3"></td><input type="hidden" name="sellPriceCHF">                     
@@ -199,7 +199,7 @@
         </tr>
 
         <tr>
-            <form name="sellRUBForm" method="POST" action="../e-counter-kamil-ducz-async/SellOperations/sellOperationRUB.php">
+            <form name="sellRUBForm" method="POST" action="SellOperations/sellOperationRUB.php">
 
                 <td id="codeSale4"></td>
                 <td id="sellPrice4"></td><input type="hidden" name="sellPriceRUB">                     
@@ -213,7 +213,7 @@
         </tr>
 
         <tr>
-            <form name="sellCZKForm" method="POST" action="../e-counter-kamil-ducz-async/SellOperations/sellOperationCZK.php">
+            <form name="sellCZKForm" method="POST" action="SellOperations/sellOperationCZK.php">
 
                 <td id="codeSale5"></td>
                 <td id="sellPrice5"></td><input type="hidden" name="sellPriceCZK">                     
@@ -227,7 +227,7 @@
         </tr>
 
         <tr>
-            <form name="sellGBPForm" method="POST" action="../e-counter-kamil-ducz-async/SellOperations/sellOperationGBP.php">
+            <form name="sellGBPForm" method="POST" action="SellOperations/sellOperationGBP.php">
 
                 <td id="codeSale6"></td>
                 <td id="sellPrice6"></td><input type="hidden" name="sellPriceGBP">                     
